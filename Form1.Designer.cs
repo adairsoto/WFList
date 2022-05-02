d876369ac2985a6b79947e7c7b104aa0755cd906
Adair Soto <adair.soto@protonmail.com>
2022-05-02 08:43:34 -0300

    1st commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Class3_ListWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAddRange = new System.Windows.Forms.Button();
+            this.btnRemoveAt = new System.Windows.Forms.Button();
+            this.btnRemByName = new System.Windows.Forms.Button();
+            this.txtRemByName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnReverse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(39, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 328);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(267, 57);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(150, 30);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(267, 159);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(150, 30);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove Range";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAddRange
+            // 
+            this.btnAddRange.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddRange.Location = new System.Drawing.Point(267, 93);
+            this.btnAddRange.Name = "btnAddRange";
+            this.btnAddRange.Size = new System.Drawing.Size(150, 30);
+            this.btnAddRange.TabIndex = 3;
+            this.btnAddRange.Text = "Add Range";
+            this.btnAddRange.UseVisualStyleBackColor = true;
+            this.btnAddRange.Click += new System.EventHandler(this.btnAddRange_Click);
+            // 
+            // btnRemoveAt
+            // 
+            this.btnRemoveAt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoveAt.Location = new System.Drawing.Point(267, 195);
+            this.btnRemoveAt.Name = "btnRemoveAt";
+            this.btnRemoveAt.Size = new System.Drawing.Size(150, 30);
+            this.btnRemoveAt.TabIndex = 4;
+            this.btnRemoveAt.Text = "Remove At";
+            this.btnRemoveAt.UseVisualStyleBackColor = true;
+            this.btnRemoveAt.Click += new System.EventHandler(this.btnRemoveAt_Click);
+            // 
+            // btnRemByName
+            // 
+            this.btnRemByName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemByName.Location = new System.Drawing.Point(267, 231);
+            this.btnRemByName.Name = "btnRemByName";
+            this.btnRemByName.Size = new System.Drawing.Size(150, 30);
+            this.btnRemByName.TabIndex = 5;
+            this.btnRemByName.Text = "Remove By Name";
+            this.btnRemByName.UseVisualStyleBackColor = true;
+            this.btnRemByName.Click += new System.EventHandler(this.btnRemByName_Click);
+            // 
+            // txtRemByName
+            // 
+            this.txtRemByName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRemByName.Location = new System.Drawing.Point(433, 234);
+            this.txtRemByName.Name = "txtRemByName";
+            this.txtRemByName.Size = new System.Drawing.Size(150, 24);
+            this.txtRemByName.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(267, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnReverse
+            // 
+            this.btnReverse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReverse.Location = new System.Drawing.Point(267, 328);
+            this.btnReverse.Name = "btnReverse";
+            this.btnReverse.Size = new System.Drawing.Size(150, 30);
+            this.btnReverse.TabIndex = 8;
+            this.btnReverse.Text = "Reverse";
+            this.btnReverse.UseVisualStyleBackColor = true;
+            this.btnReverse.Click += new System.EventHandler(this.btnReverse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 450);
+            this.Controls.Add(this.btnReverse);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtRemByName);
+            this.Controls.Add(this.btnRemByName);
+            this.Controls.Add(this.btnRemoveAt);
+            this.Controls.Add(this.btnAddRange);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAddRange;
+        private System.Windows.Forms.Button btnRemoveAt;
+        private System.Windows.Forms.Button btnRemByName;
+        private System.Windows.Forms.TextBox txtRemByName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnReverse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class3_ListWF
+{
+    public partial class Form1 : Form
+    {
+                
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            List<string> listString = new List<string>();
+            listString.Add("BMW");
+            listString.Add("Mercedes-Benz");
+            listString.Add("Audi");
+            listString.Add("Tesla");
+            listString.Add("Porsche");
+           
+            foreach (string ls in listString)
+            {
+                listBox1.Items.Add(ls); 
+            }
+        }
+
+        private void btnAddRange_Click(object sender, EventArgs e)
+        {
+            List<string> listString = new List<string>();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                listString.Add(listBox1.Items[i].ToString());
+            }
+            listBox1.Items.Clear();
+            List<string> list2 = new List<string>();
+            list2.Add("Lamborghini");
+            list2.Add("Ferrari");
+            list2.Add("Jaguar");
+            list2.Add("Maserati");
+            list2.Add("Bugatti");
+            listString.AddRange(list2);
+            foreach (string ls in listString)
+            {
+                listBox1.Items.Add(ls);
+            }
+        }
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            List<string> listString = new List<string>();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                listString.Add(listBox1.Items[i].ToString());
+            }
+            listBox1.Items.Clear();
+            listString.RemoveRange(2,6);
+            foreach (string ls in listString)
+            {
+                listBox1.Items.Add(ls);
+            }
+        }
+
+        private void btnRemoveAt_Click(object sender, EventArgs e)
+        {
+            List<string> listString = new List<string>();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                listString.Add(listBox1.Items[i].ToString());
+            }            
+            listString.RemoveAt(listBox1.SelectedIndex);
+            listBox1.Items.Clear();
+            foreach (string ls in listString)
+            {
+                listBox1.Items.Add(ls);
+            }
+        }
+
+        private void btnRemByName_Click(object sender, EventArgs e)
+        {
+            List<string> listString = new List<string>();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                listString.Add(listBox1.Items[i].ToString());
+            }
+            listString.Remove(txtRemByName.Text);
+            listBox1.Items.Clear();
+            txtRemByName.Clear();
+            foreach (string ls in listString)
+            {
+                listBox1.Items.Add(ls);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> listString = new List<string>();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                listString.Add(listBox1.Items[i].ToString());
+            }
+            listString.Sort();
+            listBox1.Items.Clear();
+            foreach (string ls in listString)
+            {
+                listBox1.Items.Add(ls);
+            }
+        }
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            List<string> listString = new List<string>();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                listString.Add(listBox1.Items[i].ToString());
+            }
+            listString.Reverse();
+            listBox1.Items.Clear();
+            foreach (string ls in listString)
+            {
+                listBox1.Items.Add(ls);
+            }
+        }
+    }
+}
